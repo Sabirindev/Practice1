9d9d30d36bd3d599172a57842e5999b7425c6f32
sabir mustafayev <sdetinscrum@gmail.com>
2021-08-07 12:42:36 -0400

    change on line 7 from sabir local

--- src/day1/Car.java
+++ src/day1/Car.java
@@ -3,5 +3,7 @@ package day1;
 public class Car {
     public static void main(String[] args) {
         System.out.println("it is beginning");
+
+        System.out.println("change on line 7 from sabir local");
     }
 }
